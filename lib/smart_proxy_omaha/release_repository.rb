1dba32c5dc60943d01f8ee92c03488fc22be633b
Timo Goebel <mail@timogoebel.name>
2017-09-18 22:55:59 +0200

    repository: filter current symlink

--- lib/smart_proxy_omaha/release_repository.rb
+++ lib/smart_proxy_omaha/release_repository.rb
@@ -1,7 +1,7 @@
 module Proxy::Omaha
   class ReleaseRepository
     def releases(track, architecture)
-      Dir.glob(File.join(Proxy::Omaha::Plugin.settings.contentpath, track, architecture, '*')).select {|f| File.directory? f }.map { |f| Gem::Version.new(File.basename(f)) }
+      Dir.glob(File.join(Proxy::Omaha::Plugin.settings.contentpath, track, architecture, '*')).select { |f| File.directory?(f) && ! File.symlink?(f) }.map { |f| Gem::Version.new(File.basename(f)) }
     end
 
     def latest_os(track, architecture)
